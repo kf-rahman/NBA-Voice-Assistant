1ec61f65135b6c44b89d08402c763db5dc9ab06f
spodamon <kazif2122r@gmail.com>
2021-05-29 20:00:55 -0300

    Added voice control

--- nbavc.py
+++ nbavc.py
@@ -3,6 +3,8 @@ import json
 import pyttsx3
 import speech_recognition as sr
 import re
+import threading
+import time
 
 API_KEY = 'tA0_xgVpoaiH'
 PROJECT_TOKEN = 'tKijAox01o6c'
@@ -17,13 +19,14 @@ class Data:
             "api_key": self.api_key
 
         }
-        self.get_data()
+        self.data = self.get_data()
 
     def get_data(self):
         response = requests.get(f'https://parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',
                                 params={'api_key': API_KEY})
 
-        self.data = json.loads(response.text)
+        data = json.loads(response.text)
+        return data
 
     def all_teams(self):#list of all the teams that are playing
         teamsname = self.data['team1']
@@ -35,13 +38,30 @@ class Data:
 
         return versus
 
+    def update_data(self):
+        response = requests.post(f'https://parsehub.com/api/v2/projects/{self.project_token}/run',params = self.params)
+
+
+
+        def poll():
+            time.sleep(0.1)
+            old_data = self.data
+            while True:
+                new_data = self.get_data()
+                if new_data != old_data:
+                    self.data = new_data
+                    print("Data Updated")
+                    break
+                time.sleep(5)
+
+        t = threading.Thread(target=poll)
+        t.start()
 
 
 
 
-data = Data(API_KEY,PROJECT_TOKEN)
-print(data.data)
-print(data.all_teams())
+
+
 #Voice
 def speak(text):
     engine = pyttsx3.init()
@@ -61,19 +81,37 @@ def get_audio():
     return said.lower()
 
 
-def main():
-    print("Program Started")
-    END_PHRASE = "stop"
 
+def main():
+    data = Data(API_KEY, PROJECT_TOKEN)
+    print("Program Started")
+    END_PHRASE ='stop'
+    result = None
+
+    TOTAL_PATTERNS = {re.compile("[\w\s]+ teams [\w\s]+playing"):data.all_teams,re.compile("[\w\s]+playing"): data.all_teams
+    }
+
+
+    UPDATE_COMMAND = "update"
     while True:
         print("Speak")
         text = get_audio()
+        print(text)
+
+        for pattern,func in TOTAL_PATTERNS.items():
+            if pattern.match(text):
+                result = func()
+                break
+        if text ==  UPDATE_COMMAND:
+            result = "Data is being updated"
+            data.update_data()
+
+        if result:
+            print(result)
+            speak(result)
 
         if text.find(END_PHRASE):
             break
 
 
-
-#To do:
-#Initialize regex patterns
-#Test speech recognition
+main()
